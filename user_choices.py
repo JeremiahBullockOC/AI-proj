235717a09a5d9c8e5e84bfadb2ce8389f1e33fa6
Jeremiah Bullock <jeremiah.r.bullock-1@ou.edu>
2023-05-09 14:09:34 -0500

    Added a user choices class for tracking and obtained button code.

---
+++ user_choices.py
@@ -0,0 +1,21 @@
+# This file is to hold and store all the choices that a user might make for a game. This will be passed to Game.py
+
+# Assisted or Unassisted
+    # If assisted navigate to algorithm of choice
+
+# Alogrithm of choice
+    # A*
+    # DFS
+    # UCS
+    # Note sure how much difference the user will see
+
+# Automated or manual
+    # If automated pause for a moment so user can see initial state.
+
+# Small or Big Maze
+    # 10x10 or 20x20
+
+# Color Theme
+    # Basic - Black and White
+    # Retro - Green lines, black background, and red walls
+    # Oceano - Deep blue background, light blue lines, Gray walls
